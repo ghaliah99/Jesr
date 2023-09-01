575ad64336e2b2a5853e8d771ab6c74965d5539e
ghaliah99 <114148086+ghaliah99@users.noreply.github.com>
2023-09-01 15:54:19 +0300

    LOGIN interface

--- lib/Screens/signIn.dart
+++ lib/Screens/signIn.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:jesr/utils/colors_utils.dart';
-
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
@@ -10,6 +9,185 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        //logo
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              	            Container(
+	              height: 400,
+	              decoration: BoxDecoration(
+	                image: DecorationImage(
+	                  image: AssetImage('assets/images/background.png'),
+	                  fit: BoxFit.fill
+	                )
+                ),
+ child: Stack(
+	                children: <Widget>[
+
+  Positioned(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 100),
+                        child: Center(
+                          child: Text(
+                            "Welcome to JESR",
+                            style: TextStyle(
+                              color:Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Ubuntu',
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+	                ],
+	              ),
+              ),
+
+Container(
+child: Stack(
+	                children: <Widget>[
+
+  Positioned(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 00),
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              color:hexStringToColor("9982C2"),
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Ubuntu',
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+	                ],
+	              ),
+),
+              Padding( 
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromRGBO(143, 148, 251, 0.2),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.grey[100]!,
+                                ),
+                              ),
+                            ),
+                            child: TextField(
+                                  style: TextStyle(fontFamily: 'Ubuntu'), // Apply Ubuntu font family
+
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Email",
+                                hintStyle: TextStyle(color: Colors.grey[550]),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                                  style: TextStyle(fontFamily: 'Ubuntu'), // Apply Ubuntu font family
+
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(color: Colors.grey[550]),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                          colors: [
+                            hexStringToColor("9982C2"),
+                            hexStringToColor("9982C2"),
+                          ],
+                        ),
+                      ),
+
+                      
+                      //BUTTON
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Add your login button onPressed logic here
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                        ),
+                        child: Center(
+                          child: Text(
+      
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Ubuntu',
+
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 70),
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Color.fromRGBO(143, 148, 251, 1),
+                        fontFamily: 'Ubuntu',
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+  /*
   TextEditingController _emailTextController = TextEditingController();
   TextEditingController _passwordTextController = TextEditingController();
 @override
@@ -113,4 +291,4 @@ TextField reusableTextField(
     }
 
 // You can keep the hexStringToColor function implementation here or import it.
-}
+}*/
